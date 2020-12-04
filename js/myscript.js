573049a5103380b67364fd8933b09478214f5e48
Emilo <emilio.furnari87@gmail.com>
2020-12-04 16:25:37 +0100

    stampato su html

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,12 @@
-alert("Hello world!");
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un’immagine presa anch’essa da un data
+
+
+var app = new Vue(
+   {
+     el: '#root',
+     data: {
+       message: "Hello World"
+     }
+   }
+);
